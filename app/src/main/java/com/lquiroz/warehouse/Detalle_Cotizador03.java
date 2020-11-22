6c2feadcce447aff9f38d72bd995894ba4d55818
Mario Castro <mcastroascencio@gmail.com>
2020-11-22 15:20:51 -0500

    Detalle Cotizador

---
+++ app/src/main/java/com/lquiroz/warehouse/Detalle_Cotizador03.java
@@ -0,0 +1,14 @@
+package com.lquiroz.warehouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detalle_Cotizador03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle__cotizador03);
+    }
+}
