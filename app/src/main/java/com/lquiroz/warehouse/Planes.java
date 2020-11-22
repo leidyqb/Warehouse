1dda8d7bfe9cd8aaba0fef7d2106e8c16b322ac5
Mario Castro <mcastroascencio@gmail.com>
2020-11-22 13:58:58 -0500

    Pantalla Planes Disponibles

---
+++ app/src/main/java/com/lquiroz/warehouse/Planes.java
@@ -0,0 +1,14 @@
+package com.lquiroz.warehouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Planes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planes);
+    }
+}
