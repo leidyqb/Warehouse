0845ac6e2c59861fa21ff2e6dbefbd9644b5ea33
Mario Castro <mcastroascencio@gmail.com>
2020-12-01 13:26:58 -0500

    integracion mapa

--- app/src/main/java/com/lquiroz/warehouse/Planes.java
+++ app/src/main/java/com/lquiroz/warehouse/Planes.java
@@ -6,13 +6,29 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 
 public class Planes extends AppCompatActivity {
+
+    Button btnUbicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planes);
+        asignarReferencias();
+    }
+
+    private void asignarReferencias(){
+        btnUbicacion = findViewById(R.id.btnUbicacion);
+        btnUbicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Planes.this,UbicacionActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     public void  llamar (View view){
@@ -25,5 +41,6 @@ public class Planes extends AppCompatActivity {
     }
 
 }
+
 
 
